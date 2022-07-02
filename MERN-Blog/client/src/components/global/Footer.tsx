4680af2b73a5d77ac6bb91076d3b16be47e3bd6a
Raghav Jindal <raghavjindal131@gmail.com>
2022-07-02 12:17:51 +0530

    Add files via upload

---
+++ MERN-Blog/client/src/components/global/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      Major 2 made with ❤
+    </div>
+  )
+}
+
+export default Footer
